ca06167ef5b167a6f37c41ccd6666db711317174
Dave Richards <dadeece@gmail.com>
2015-08-25 19:12:11 +0100

    Modified the ringing generator to use the library functions

--- src/genringing.m
+++ src/genringing.m
@@ -381,107 +381,16 @@ bell_count = size(rows,2);
 
 function writeLowndesFile( lowndes_file, strikes)
 
-lowndes_fid = fopen(lowndes_file, 'w');
-if (lowndes_fid>=0)
-    % Strike data is rows of:
-    %   Time Bell Hand/back
-    firstStrike = strikes(1).actual_time;
+lowndes_data.strike = strikes;
+lowndes_data.info.creator = 'Genringing';
 
-    % Write a header - just copied from Hawkear output
-    fprintf(lowndes_fid, '#. Lowndes: Version 2 (support comments in the file)\n');
-    fprintf(lowndes_fid, '#. Creator: genRinging Version 0.1\n');
-    fprintf(lowndes_fid, '#. TranscriptionDate: %s\n', datestr(now, 'yyyy-mm-dd--HH-MM'));
-    fprintf(lowndes_fid, '#. FirstBlowMs: %d\n', round(1000*firstStrike));
-
-    % Write out the strikes one by one
-    for writeStrikes = 1:size(strikes,1)
-        if strikes(writeStrikes).bell>0
-            strikeTime = strikes(writeStrikes).actual_strike - firstStrike;
-            if (strikes(writeStrikes).handstroke)
-                hbIdent = 'H';
-            else
-                hbIdent = 'B';
-            end
-            bellIds = { '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O', 'E', 'T' };
-            fprintf(lowndes_fid, '%s %s 0X%04x\n', hbIdent, bellIds{ strikes(writeStrikes).bell }, mod( round(1000*strikeTime), 65536) );
-        end
-    end
-else
-    warning('genringing: could not create Lowndes file %s', lowndes_file)
-end
-fclose(lowndes_fid);
-
+lowndes.write( lowndes_file, lowndes_data);
 
 function writeXMLFile( xml_file, strikes, NUM_BELLS, VERSION)
 
 SOURCE_NAME = 'RingingSim';
 
-documentNode = com.mathworks.xml.XMLUtils.createDocument('transcription');
-
-rootNode = documentNode.getDocumentElement;
-rootNode.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-rootNode.setAttribute('xsi:schemaLocation', 'http://drichards2.github.io/toast-visualiser/xsd/enhanced-strike-definition.xsd');
-rootNode.setAttribute('version', '1');
-
-datasources = documentNode.createElement('dataSources');
-sim_datasource = documentNode.createElement('dataSource');
-sim_datasource_name = documentNode.createElement('name');
-sim_datasource_name.appendChild(documentNode.createTextNode('Matlab ringing simulator'));
-sim_datasource_version = documentNode.createElement('version');
-sim_datasource_version.appendChild(documentNode.createTextNode(VERSION));
-sim_datasource_comment = documentNode.createElement('comment');
-sim_datasource_comment.appendChild(documentNode.createTextNode('Matlab ringing simulator - fantastic things are about to happen'));
-sim_datasource.appendChild( sim_datasource_name );
-datasources.appendChild(sim_datasource);
-rootNode.appendChild(datasources);
-
-renderlayers = documentNode.createElement('renderLayers');
-visualisation = documentNode.createElement('visualisation');
-visualisation_name = documentNode.createElement('name');
-visualisation_name.appendChild(documentNode.createTextNode('Matlab Ringing Simulator'));
-visualisation_description = documentNode.createElement('description');
-visualisation_description.appendChild(documentNode.createTextNode('Nothing to see here'));
-visualisation_prioritylist = documentNode.createElement('priorityList');
-visualisation_prioritylist_source = documentNode.createElement('source');
-visualisation_prioritylist_source.appendChild(documentNode.createTextNode(SOURCE_NAME));
-visualisation_prioritylist.appendChild(visualisation_prioritylist_source);
-
-visualisation.appendChild(visualisation_name);
-visualisation.appendChild(visualisation_description);
-visualisation.appendChild(visualisation_prioritylist);
-renderlayers.appendChild(visualisation);
-
-rootNode.appendChild(renderlayers);
-
-strikedata  = documentNode.createElement('strikeData');
-for runstrikes = 1:length(strikes)
-    if mod(runstrikes, NUM_BELLS) == 1
-        rowDelimiter = documentNode.createElement('rowDelimiter');
-        rowDelimiter.setAttribute('source', SOURCE_NAME);
-        strikedata.appendChild(rowDelimiter);
-    end
-    strike_node = documentNode.createElement('strike');
-    strike_bell = documentNode.createElement('bell');
-    strike_bell.appendChild(documentNode.createTextNode(sprintf('%d', strikes(runstrikes).bell)));
-    strike_node.appendChild( strike_bell );
-    strike_original = documentNode.createElement('original');
-    strike_original.appendChild(documentNode.createTextNode(sprintf('%.3f', strikes(runstrikes).actual_time)));
-    strike_node.appendChild( strike_original );
-    
-    strike_modelout = documentNode.createElement('modelOutput');
-    strike_modelout.setAttribute('source', SOURCE_NAME);
-    strike_modelout_time = documentNode.createElement('time');
-    strike_modelout_time.appendChild(documentNode.createTextNode(sprintf('%.3f', strikes(runstrikes).ideal_time)));
-    strike_modelout.appendChild( strike_modelout_time );
-    strike_node.appendChild( strike_modelout );
-    
-    strikedata.appendChild( strike_node );
-end
-
-rootNode.appendChild(strikedata);
-
-
-
+documentNode = strikex.build(strikes, SOURCE_NAME, SOURCE_NAME, 1);
 
 xmlwrite( xml_file, documentNode);
 
